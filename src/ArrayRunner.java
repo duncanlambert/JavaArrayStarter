b14a35c6e9ec60380eecb0bfe6a5b13b9f77f2e5
duncanlambert <duncan.lambert@kinkaid.org>
2019-10-01 14:28:24 -0500

    Made two students, looped through 12/all and printed them out.

--- src/ArrayRunner.java
+++ src/ArrayRunner.java
@@ -5,5 +5,16 @@ public class ArrayRunner
         Student me = new Student("Mr. Howe","December","blue");
         System.out.println(me);
 
+        Student[] myClass = new Student[12];
+        Student studentA = new Student("Isabelle", "April", "Pink");
+        myClass[2] = studentA;
+        Student studentB = new Student("Lydia", "October", "Purple");
+        myClass[0] = studentB;
+
+        for (int i = 0; i<12; i++)
+        {
+            System.out.println(myClass[i]);
+        }
+
     }
 }
